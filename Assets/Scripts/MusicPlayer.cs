a792fc5717f1fe59f5639105f5d0380687f9d674
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-19 19:04:44 +0800

    Added new background music for each scene. Added background animations to each menu screen.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
 	
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+	
+	private AudioSource music;
+	
 	void Start () {
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
@@ -11,7 +17,35 @@ public class MusicPlayer : MonoBehaviour {
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
 		}
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		Debug.Log("Level loaded: " + level);
 		
+		if (music != null) {
+			music.Stop();
+				
+			switch (level) {
+				// Start menu
+				case 0:
+					music.clip = startClip;
+					break;
+				// Game screen
+				case 1:
+					music.clip = gameClip;
+					break;
+				// Win/Lose Screen
+				default:
+					music.clip = endClip;
+					break;
+			}
+			music.loop = true;
+			music.Play();
+		}
 	}
 }
